bd99d606881908cbe5611bbca3eea6fac99d594a
ValidSmile77 <ValidSmile2@gmail.com>
2024-06-26 02:29:59 -0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('confirm-button').addEventListener('click', function() {
-    const password = document.getElementById('password').value;
-    if (password === 'RETROSTUDIOSOVERLORD') {
-        window.location.href = 'video.html';
-    } else {
-        alert('Incorrect password');
-    }
-});
